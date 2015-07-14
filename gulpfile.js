7ad3721ce1474093f53481799a25078f35874642
Mehta, Devraj <Devraj.Mehta@gtri.gatech.edu>
2015-07-14 08:51:19 -0400

    remove caching from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,11 @@ var gulp = require('gulp'),
     lazypipe = require('lazypipe'),
     src = {},
     templates,
-    through2 = require('through2'),
     $ = require('gulp-load-plugins')();
-
-function cleanCache(cacheName) {
-    return lazypipe().pipe(through2.obj, function (file, enc, cb) {
-        if (file.event === 'unlink') {
-            delete $.cached.caches[cacheName][file.path];
-            $.remember.forget(cacheName, file.path);
-        } else {
-            this.push(file);
-        }
-        cb();
-    });
-}
 
 src.templates = 'src/**/*.html';
 templates = lazypipe()
     .pipe($.debug, {title: 'from pipe', minimal: false})
-    .pipe($.cached, 'templates')
-    .pipe($.remember, 'templates')
-    .pipe($.debug, {title: 'after remember', minimal: false})
     .pipe($.angularTemplatecache)
     .pipe(gulp.dest, 'dist/');
 
@@ -33,7 +17,7 @@ gulp.task('templates', function () {
 gulp.task('default', ['templates'], function () {
     $.watch(src.templates, $.batch(function (events, done) {
         events
-            .pipe(cleanCache('templates').pipe(templates)())
+            .pipe(templates())
             .pipe($.util.buffer(function () {
                 done();
             }));
